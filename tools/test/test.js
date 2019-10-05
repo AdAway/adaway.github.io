8cec55bfed08de34746dfdd0b0be2be691a7863f
PerfectSlayer <bruce.bujon@gmail.com>
2019-10-05 16:45:07 +0200

    feat: group domains wathever their TLDs

--- tools/test/test.js
+++ tools/test/test.js
@@ -27,5 +27,35 @@ describe('domains', function () {
 
             assert.deepEqual(sourceDomains, sortedDomains);
         });
+
+        it('should group domain whatever their TLD', function () {
+            var sourceDomains = [
+                "content.ad",
+                "token.pub",
+                "content.pub",
+                "token.ad",
+                "api.content.ad",
+                "api.content.pub"
+            ];
+            var sortedDomains = [
+                "content.ad",
+                "api.content.ad",
+                "content.pub",
+                "api.content.pub",
+                "token.ad",
+                "token.pub"
+            ];
+
+            sourceDomains = sourceDomains.map(domain => {
+                return {
+                    host: domain,
+                    domain: parseDomain(domain)
+                }
+            });
+            sourceDomains.sort(domains.compare);
+            sourceDomains = sourceDomains.map(domain => domain.host);
+
+            assert.deepEqual(sourceDomains, sortedDomains);
+        });
     });
 });
